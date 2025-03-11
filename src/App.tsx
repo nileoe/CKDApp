f48c84a6d5a88fc42bfd48893f83d40f8980217d
nileoe <linoperdrix@gmail.com>
2025-03-11 11:59:22 +0000

    added basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { User } from "./types";
+import Home from "./screens/Home";
+import Layout from "./layouts/Layout";
+
 function App() {
-  return <p>app will be there :')</p>;
+  //return <TestComponent />;
+  const loggedInUser: User = { id: 1234, username: "kingCharles" };
+  return (
+    <BrowserRouter>
+      <Layout loggedInUser={loggedInUser}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
