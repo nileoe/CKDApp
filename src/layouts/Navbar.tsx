571c57bec1a957fb8e27a3956301fff118a27c22
nileoe <linoperdrix@gmail.com>
2025-03-13 17:11:33 +0000

    added icons, separated navbar in 2 areas

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//import { faUser } from '@fortawesome/free-solid-svg-icons';
-//import './Navbar.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.scss";
 //import { useAuth } from '../auth/useAuth';
 //
 function Navbar() {
@@ -10,162 +10,30 @@ function Navbar() {
   // activeClassName = the current URL matches the one in 'exact'
   return (
     <nav>
-      <NavLink to="/calculator">Calculator</NavLink>
-      <NavLink to="/past_results">Past Results</NavLink>
-      <NavLink to="/consultation_notes">Consultation Notes</NavLink>
-      <NavLink to="/CKD_stages">CKD Stages</NavLink>
+      <div className="navStart">
+        <div className="navItem">
+          <NavLink to="/calculator">Calculator</NavLink>
+        </div>
+        <div className="navItem">
+          <NavLink to="/past_results">Past Results</NavLink>
+        </div>
+        <div className="navItem">
+          <NavLink to="/consultation_notes">Consultation Notes</NavLink>
+        </div>
+        <div className="navItem">
+          <NavLink to="/CKD_stages">CKD Stages</NavLink>
+        </div>
+      </div>
+
+      <div className="navEnd">
+        <div className="navItem">
+          <NavLink to="/account">
+            <FontAwesomeIcon icon={faUser} />
+          </NavLink>
+        </div>
+      </div>
     </nav>
   );
 }
 
 export default Navbar;
-//
-//// //lino
-//// <>
-//// <nav>
-////   <div className="navItem">
-////     <NavLink exact to="/" activeClassName="active">
-////       Home
-////     </NavLink>
-////   </div>
-////   {!loggedInUser ? (
-////     <div className="navItem" id="logIn">
-////       <NavLink to="/login" activeClassName="active">
-////         Login
-////       </NavLink>
-////     </div>
-////   ) : (
-////     <div className="navItem" id="acc">
-////       <NavLink to="/account" activeClassName="active">
-////         <FontAwesomeIcon icon={faUser} />
-////       </NavLink>
-////     </div>
-////   )}
-////   {loggedInUserIsAdmin ? (
-////     <div className="navItem">
-////       <NavLink exact to="/modules" activeClassName="active">
-////         Modules
-////       </NavLink>
-////     </div>
-////   ) : null}
-////   {loggedInUser && !loggedInUserIsAdmin && (
-////     <div className="navItem">
-////       <NavLink to="/projects" activeClassName="active">
-////         Projects
-////       </NavLink>
-////     </div>
-////   )}
-//// </nav>
-//// </>
-//
-//// //OLD
-////      <nav>
-////        <div className="navItem">
-////          <NavLink exact to="/" activeClassName="active">
-////            Home
-////          </NavLink>
-////        </div>
-////
-////        <div className="navItem">
-////          <NavLink exact to="/modules" activeClassName="active">
-////            Modules
-////          </NavLink>
-////        </div>
-////
-////        {loggedInUser && (
-////          <div className="navItem">
-////            <NavLink to="/projects" activeClassName="active">
-////              Projects
-////            </NavLink>
-////          </div>
-////        )}
-////
-////        {loggedInUser && (
-////          <div className="navItem">
-////            <NavLink exact to="/pie" activeClassName="active">
-////              Graphs
-////            </NavLink>
-////          </div>
-////        )}
-////
-////        {/* this will be later implemented with admin only can see this  */}
-////        {loggedInUser && (
-////          <div className="navItem">
-////            <NavLink exact to="/Overview" activeClassName="active">
-////              Overview
-////            </NavLink>
-////          </div>
-////        )}
-////
-////        {!loggedInUser ? (
-////          <div className="navItem" id="logIn">
-////            <NavLink to="/login" activeClassName="active">
-////              <FontAwesomeIcon icon={faUser} />
-////            </NavLink>
-////          </div>
-////        ) : (
-////          <div className="navItem" id="acc">
-////            <NavLink to="/account" activeClassName="active">
-////              <FontAwesomeIcon icon={faUser} />
-////            </NavLink>
-////          </div>
-////        )}
-////      </nav>
-////    </>
-////
-////
-////
-////
-////
-////
-////
-////
-////
-////
-////
-////
-////
-////
-////
-////
-////
-////return (
-////    <>
-////      <nav>
-////        <div className="navItem">
-////          <NavLink exact to="/" activeClassName="active">
-////            Home
-////          </NavLink>
-////        </div>
-////	{ !loggedInUser?
-////	      (
-////		<div className="navItem" id="logIn">
-////		  <NavLink to="/login" activeClassName="active">
-////		    <FontAwesomeIcon icon={faUser} />
-////		  </NavLink>
-////		</div>
-////	      ) :
-////	      (
-////		<div className="navItem" id="acc">
-////		  <NavLink to="/account" activeClassName="active">
-////		    <FontAwesomeIcon icon={faUser} />
-////		  </NavLink>
-////		</div>
-////		    loggedInUserIsAdmin? (
-////
-////		    <div className="navItem">
-////		      <NavLink exact to="/modules" activeClassName="active">
-////			Modules
-////		      </NavLink>
-////		    </div>
-////		    ) :
-////		    (
-////		      <div className="navItem">
-////			<NavLink to="/projects" activeClassName="active">
-////			  Projects
-////			</NavLink>
-////		      </div>
-////		    )
-////	      ) }
-////      </nav>
-////  );
