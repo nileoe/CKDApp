a5f13ec8f021446afbcaf57cba77034c713bdaba
nileoe <linoperdrix@gmail.com>
2025-03-13 13:24:41 +0000

    added mock screens

---
+++ src/screens/PastResultsScreen.tsx
@@ -0,0 +1,23 @@
+const PastResultsScreen = () => {
+  // initialization -----------
+  // State -------------
+  // Handlers ----------
+  // View -----------
+  return (
+    <>
+      <h1>PastResultsScreen Page</h1>
+      <p>
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent risus est
+        proin eget nostra. Imperdiet auctor consectetur risus facilisi sem
+        natoque. Potenti magnis dignissim enim proin integer et. Litora hac
+        metus libero ex nostra fusce hendrerit. Afeugiat eleifend finibus
+        feugiat bibendum sagittis litora fringilla. Volutpat senectus elementum
+        auctor vitae eros lectus mattis. Maximus posuere vel neque erat
+        suscipit, magna placerat. Massa lobortis luctus pharetra ornare vitae
+        et. Mattis habitant sit porta leo netus odio.
+      </p>
+    </>
+  );
+};
+
+export default PastResultsScreen;
