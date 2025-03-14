6bcf060ec0e6a70cb2845d11793b076e51128f2e
nileoe <linoperdrix@gmail.com>
2025-03-14 19:43:12 +0000

    created calculator form

---
+++ src/entity/CalculatorForm.tsx
@@ -0,0 +1,15 @@
+import "./CalculatorForm.scss";
+
+const CalculatorForm = () => {
+  // initialization -----------
+  // State -------------
+  // Handlers ----------
+  // View -----------
+  return (
+    <>
+      <p>calculation form</p>
+    </>
+  );
+};
+
+export default CalculatorForm;
--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,3 +1,5 @@
+import CalculatorForm from "../entity/CalculatorForm";
+
 const CalculatorScreen = () => {
   // initialization -----------
   // State -------------
@@ -6,16 +8,7 @@ const CalculatorScreen = () => {
   return (
     <>
       <h1>CalculatorScreen Page</h1>
-      <p>
-        Lorem ipsum odor amet, consectetuer adipiscing elit. Praesent risus est
-        proin eget nostra. Imperdiet auctor consectetur risus facilisi sem
-        natoque. Potenti magnis dignissim enim proin integer et. Litora hac
-        metus libero ex nostra fusce hendrerit. Afeugiat eleifend finibus
-        feugiat bibendum sagittis litora fringilla. Volutpat senectus elementum
-        auctor vitae eros lectus mattis. Maximus posuere vel neque erat
-        suscipit, magna placerat. Massa lobortis luctus pharetra ornare vitae
-        et. Mattis habitant sit porta leo netus odio.
-      </p>
+      <CalculatorForm />
     </>
   );
 };
